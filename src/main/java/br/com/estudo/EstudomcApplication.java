e353b7036bb20fbabffde59900f78e2163f325c2
Sergio Ovalle <sergioomarcelo@gmail.com>
2020-06-04 10:36:48 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/estudo/EstudomcApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudomcApplication.class, args);
+	}
+
+}
