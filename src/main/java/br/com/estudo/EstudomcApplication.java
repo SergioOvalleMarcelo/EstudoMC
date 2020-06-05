4e3534a3e670e50a1fc6c55b531118ff70f5ecf4
Sergio Ovalle <sergioomarcelo@gmail.com>
2020-06-05 15:38:10 -0300

    Implementação oara salvar categorias na execucao do projeto

--- src/main/java/br/com/estudo/EstudomcApplication.java
+++ src/main/java/br/com/estudo/EstudomcApplication.java
@@ -1,13 +1,33 @@
 package br.com.estudo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudomcApplication {
+import br.com.estudo.domain.Categoria;
+import br.com.estudo.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class EstudomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria objCategoria1 = new Categoria(null, "Informática");
+		Categoria objCategoria2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(objCategoria1, objCategoria2));
+		
 	}
 
 }
