cc5a78285a4b9d4d6307b66cd7fdb1851f94f6aa
Sergio Ovalle <sergioomarcelo@gmail.com>
2020-06-10 11:46:32 -0300

    Implementacao do Endpoint /pedidos/{id}

---
+++ src/main/java/br/com/estudo/services/PedidoService.java
@@ -0,0 +1,26 @@
+package br.com.estudo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.estudo.domain.Pedido;
+import br.com.estudo.repositories.PedidoRepository;
+import br.com.estudo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+//		Pedido objPedido = repo.findById(id).get();
+		Optional<Pedido> objPedido = repo.findById(id);
+
+		return objPedido.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	};
+
+}
